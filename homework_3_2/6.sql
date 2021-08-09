8d27e33a506fb3b1d54accd97900c6795cde718c
KrutkoOleksii <78808009+KrutkoOleksii@users.noreply.github.com>
2021-08-09 14:00:45 +0300

    Update 6.sql

--- homework_3_2/6.sql
+++ homework_3_2/6.sql
@@ -1,12 +1,12 @@
-select
+SELECT
  project_name,
- sum(cost) as cost,
- avg(salary) as average_salary
- from developers
- inner join developer_project
- on developer_project.developer_id = developers.developer_id
- inner join projects
- on developer_project.project_id = projects.project_id
- group by project_name
+ SUM(cost) AS cost,
+ AVG(salary) as average_salary
+ FROM developers
+ INNER JOIN developer_project
+ ON developer_project.developer_id = developers.developer_id
+ INNER JOIN projects
+ ON developer_project.project_id = projects.project_id
+ GROUP BY project_name
  ORDER BY cost
  LIMIT 1
