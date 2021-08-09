de4ac8005359359ace4679df2297378026e75179
KrutkoOleksii <78808009+KrutkoOleksii@users.noreply.github.com>
2021-08-09 13:59:23 +0300

    Update 3.sql

--- homework_3_2/3.sql
+++ homework_3_2/3.sql
@@ -1,13 +1,13 @@
-select
-sum(salary) as amount_salary_Java
-from(
-select
- distinct developer_name,
+SELECT
+sum(salary) AS amount_salary_Java
+FROM(
+SELECT
+ DISTINCT developer_name,
  salary
- from developers
- inner join developer_skill
- on developer_skill.developer_id = developers.developer_id
- inner join skills
- on developer_skill.skill_id = skills.skill_id
- where skill_name = 'Java'
+ FROM developers
+ INNER JOIN developer_skill
+ ON developer_skill.developer_id = developers.developer_id
+ INNER JOIN skills
+ ON developer_skill.skill_id = skills.skill_id
+ WHERE skill_name = 'Java'
  ) tb
