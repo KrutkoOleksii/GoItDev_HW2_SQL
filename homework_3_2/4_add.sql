48b78363b0c78ab411e32d04c276467cbf58a006
KrutkoOleksii <78808009+KrutkoOleksii@users.noreply.github.com>
2021-08-09 13:55:03 +0300

    Update 4_add.sql

--- homework_3_2/4_add.sql
+++ homework_3_2/4_add.sql
@@ -1,8 +1,8 @@
-UPDATE `hw2`.`projects` SET `cost` = '200000' WHERE (`project_id` = '1');
-UPDATE `hw2`.`projects` SET `cost` = '250000' WHERE (`project_id` = '2');
-UPDATE `hw2`.`projects` SET `cost` = '180000' WHERE (`project_id` = '3');
-UPDATE `hw2`.`projects` SET `cost` = '300000' WHERE (`project_id` = '4');
-UPDATE `hw2`.`projects` SET `cost` = '230000' WHERE (`project_id` = '5');
-UPDATE `hw2`.`projects` SET `cost` = '100000' WHERE (`project_id` = '6');
-UPDATE `hw2`.`projects` SET `cost` = '330000' WHERE (`project_id` = '7');
-UPDATE `hw2`.`projects` SET `cost` = '210000' WHERE (`project_id` = '8');
+UPDATE hw2.projects SET cost = 200000 WHERE (project_id = 1);
+UPDATE hw2.projects SET cost = 250000 WHERE (project_id = 2);
+UPDATE hw2.projects SET cost = 180000 WHERE (project_id = 3);
+UPDATE hw2.projects SET cost = 300000 WHERE (project_id = 4);
+UPDATE hw2.projects SET cost = 230000 WHERE (project_id = 5);
+UPDATE hw2.projects SET cost = 100000 WHERE (project_id = 6);
+UPDATE hw2.projects SET cost = 330000 WHERE (project_id = 7);
+UPDATE hw2.projects SET cost = 210000 WHERE (project_id = 8);
